fb251dd7a5d455e9e79290e175b629ab85e2f3e9
ErenAydinc <erenaydinc@hotmail.com>
2024-09-16 08:34:33 +0300

    16.09

--- src/core/EAInfrastructure.Pipelines/TranslationService.cs
+++ src/core/EAInfrastructure.Pipelines/TranslationService.cs
@@ -17,33 +17,33 @@ namespace Core.EAInfrastructure
             _httpClient = httpClient;
         }
 
-        //   public async Task<string> TranslateAsync(string text, string targetLanguage)
-        //   {
-        //       // Google Translate API için URL
-        //       var url = $"https://translation.googleapis.com/language/translate/v2?key=AIzaSyByno5di9rdYn_6dEeskN_6A8Uxqf3jvms";
+        public async Task<string> TranslateAsync(string text, string targetLanguage)
+        {
+            // Google Translate API için URL
+            var url = $"https://translation.googleapis.com/language/translate/v2?key=AIzaSyByno5di9rdYn_6dEeskN_6A8Uxqf3jvms";
 
-        //       // İstek verisi
-        //       var requestContent = new StringContent(
-        //$"{{\"q\": \"{text}\", \"target\": \"{targetLanguage}\"}}",
-        //Encoding.UTF8,
-        //"application/json");
+            // İstek verisi
+            var requestContent = new StringContent(
+     $"{{\"q\": \"{text}\", \"target\": \"{targetLanguage}\"}}",
+     Encoding.UTF8,
+     "application/json");
 
-        //       var response = await _httpClient.PostAsync(url, requestContent);
-        //       response.EnsureSuccessStatusCode();
+            var response = await _httpClient.PostAsync(url, requestContent);
+            response.EnsureSuccessStatusCode();
 
-        //       var responseJson = await response.Content.ReadAsStringAsync();
+            var responseJson = await response.Content.ReadAsStringAsync();
 
-        //       // JSON yanıtını parse ederek çevirilmiş metni çıkarın
-        //       var jsonDocument = JsonDocument.Parse(responseJson);
-        //       var translatedText = jsonDocument
-        //           .RootElement
-        //           .GetProperty("data")
-        //           .GetProperty("translations")[0]
-        //           .GetProperty("translatedText")
-        //           .GetString();
+            // JSON yanıtını parse ederek çevirilmiş metni çıkarın
+            var jsonDocument = JsonDocument.Parse(responseJson);
+            var translatedText = jsonDocument
+                .RootElement
+                .GetProperty("data")
+                .GetProperty("translations")[0]
+                .GetProperty("translatedText")
+                .GetString();
 
-        //       return translatedText;
-        //   }
+            return translatedText;
+        }
 
         //public async Task<string> TranslateAsync(string text, string targetLanguage)
         //{
